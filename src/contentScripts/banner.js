357a73b9dff524bc2c43c8753150a1be4a44b4a2
Remon van de Kamp <rpkamp@gmail.com>
2019-07-19 17:18:00 +0200

    Use shadowdom to prevent styles from bleeding into the banner

--- src/contentScripts/banner.js
+++ src/contentScripts/banner.js
@@ -2,18 +2,23 @@ window.chrome.runtime.onMessage.addListener((message) => {
   const callback = () => {
     const div = document.createElement('div');
     div.className = 'environmentIndicatorBanner';
-    div.innerHTML = message.text.replace("\n", '<br />');
 
-    div.addEventListener('click', () => {
-      if (/environmentIndicatorBanner--left/.test(div.className)) {
-        div.className = 'environmentIndicatorBanner';
-      } else {
-        div.className = 'environmentIndicatorBanner environmentIndicatorBanner--left';
-      }
-    });
+    const style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.href = chrome.runtime.getURL('contentScripts/contents.css');
+
+    const contents = document.createElement('div');
+    contents.className = 'environmentIndicatorBannerContents';
+    contents.innerHTML = message.text.replace("\n", '<br />');
+
+    const shadowRoot = div.attachShadow({mode: 'open'});
+    shadowRoot.appendChild(style);
+    shadowRoot.appendChild(contents);
 
     document.querySelector('body').appendChild(div);
-  };
+
+    div.addEventListener('click', () => div.classList.toggle('environmentIndicatorBanner--left'));
+  };1
 
   if (document.readyState !== 'loading') {
     callback();
