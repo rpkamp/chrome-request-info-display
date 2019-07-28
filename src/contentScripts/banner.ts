8456fd51fe239a34bcaf119afd6678809e8bd5a8
Remon van de Kamp <rpkamp@gmail.com>
2019-07-28 14:27:56 +0200

    Inject custom CSS on showing banner

--- src/contentScripts/banner.ts
+++ src/contentScripts/banner.ts
@@ -2,17 +2,20 @@ window.chrome.runtime.onMessage.addListener((message: any) => {
   const callback = () => {
     const div = document.createElement('div');
     div.className = 'environmentIndicatorBanner';
+    const shadowRoot = div.attachShadow({mode: 'open'});
 
-    const style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.href = chrome.runtime.getURL('contents.css');
+    const cssFile = document.createElement('link');
+    cssFile.rel = 'stylesheet';
+    cssFile.href = chrome.runtime.getURL('contents.css');
+    shadowRoot.appendChild(cssFile);
+
+    const style = document.createElement('style');
+    style.innerText = `.environmentIndicatorBannerContents { ${message.css} }`;
+    shadowRoot.appendChild(style);
 
     const contents = document.createElement('div');
     contents.className = 'environmentIndicatorBannerContents';
     contents.innerHTML = message.text.replace("\n", '<br />');
-
-    const shadowRoot = div.attachShadow({mode: 'open'});
-    shadowRoot.appendChild(style);
     shadowRoot.appendChild(contents);
 
     document.body.appendChild(div);
